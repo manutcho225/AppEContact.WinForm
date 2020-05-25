5b954b8b21b8780689eb122086c7b1e7df3e1b64
angelomanutcho228@gmail.com <angelomanutcho228@gmail.com>
2020-05-25 23:33:25 +0000

    Niveau 1 gestion de contact le 25/05/2020

---
+++ AppEContact.WinForm/Models/Contact.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppEContact.WinForm.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; } // prop + double tabulation
+        public string NomComplet { get; set; }
+        public DateTime DateNaiss { get; set; }
+        public string Email { get; set; }
+        public string Tel { get; set; }
+        public string Genre { get; set; }
+        public string UrlPhoto { get; set; }
+
+        public Contact(string nomComplet, DateTime dateNaiss, string email, string tel, string genre, string urlPhoto)
+        {
+            this.NomComplet = nomComplet;
+            this.DateNaiss = dateNaiss;
+            this.Email = email;
+            this.Tel = tel;
+            this.Genre = genre;
+            this.UrlPhoto = urlPhoto;
+        }
+
+        public Contact() //constructeur par défaut
+        {
+
+        }
+    }
+}
