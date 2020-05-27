ef741c77a85fbcda8124cfa3f2204f5a0b070b2a
angelomanutcho228@gmail.com <angelomanutcho228@gmail.com>
2020-05-27 00:15:58 +0000

    Modifier le 27/05/2020

--- AppEContact.WinForm/frmListContact.Designer.cs
+++ AppEContact.WinForm/frmListContact.Designer.cs
@@ -33,8 +33,27 @@
             this.label1 = new System.Windows.Forms.Label();
             this.nbContact = new System.Windows.Forms.Label();
             this.dgvContacts = new System.Windows.Forms.DataGridView();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtPictureContact = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtTel = new System.Windows.Forms.MaskedTextBox();
+            this.txtGenre = new System.Windows.Forms.ComboBox();
+            this.txtDateNaiss = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.txtboxID = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvContacts)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPictureContact)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox2
@@ -60,7 +79,7 @@
             // nbContact
             // 
             this.nbContact.AutoSize = true;
-            this.nbContact.Location = new System.Drawing.Point(490, 18);
+            this.nbContact.Location = new System.Drawing.Point(371, 67);
             this.nbContact.Name = "nbContact";
             this.nbContact.Size = new System.Drawing.Size(53, 20);
             this.nbContact.TabIndex = 5;
@@ -69,14 +88,226 @@
             // dgvContacts
             // 
             this.dgvContacts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvContacts.Location = new System.Drawing.Point(21, 73);
+            this.dgvContacts.Location = new System.Drawing.Point(21, 104);
             this.dgvContacts.Name = "dgvContacts";
-            this.dgvContacts.Size = new System.Drawing.Size(782, 325);
+            this.dgvContacts.Size = new System.Drawing.Size(782, 205);
             this.dgvContacts.TabIndex = 6;
+            this.dgvContacts.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvContacts_CellClick);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(21, 61);
+            this.txtSearch.Multiline = true;
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(250, 33);
+            this.txtSearch.TabIndex = 7;
+            this.txtSearch.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtSearch_KeyUp);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.Transparent;
+            this.btnSearch.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearch.Image = ((System.Drawing.Image)(resources.GetObject("btnSearch.Image")));
+            this.btnSearch.Location = new System.Drawing.Point(281, 61);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(48, 33);
+            this.btnSearch.TabIndex = 8;
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.BackColor = System.Drawing.Color.LightCoral;
+            this.btnUpload.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btnUpload.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpload.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpload.Image = ((System.Drawing.Image)(resources.GetObject("btnUpload.Image")));
+            this.btnUpload.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnUpload.Location = new System.Drawing.Point(19, 502);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(159, 44);
+            this.btnUpload.TabIndex = 29;
+            this.btnUpload.Text = "Parcourir...";
+            this.btnUpload.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnUpload.UseVisualStyleBackColor = false;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnDelete.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Image = ((System.Drawing.Image)(resources.GetObject("btnDelete.Image")));
+            this.btnDelete.Location = new System.Drawing.Point(569, 502);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(159, 44);
+            this.btnDelete.TabIndex = 28;
+            this.btnDelete.Text = "  Supprimer";
+            this.btnDelete.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.DarkCyan;
+            this.btnSave.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Image = ((System.Drawing.Image)(resources.GetObject("btnSave.Image")));
+            this.btnSave.Location = new System.Drawing.Point(351, 502);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(159, 44);
+            this.btnSave.TabIndex = 27;
+            this.btnSave.Text = "  Modifier";
+            this.btnSave.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSave.UseVisualStyleBackColor = false;
+            // 
+            // txtPictureContact
+            // 
+            this.txtPictureContact.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtPictureContact.Location = new System.Drawing.Point(21, 330);
+            this.txtPictureContact.Name = "txtPictureContact";
+            this.txtPictureContact.Size = new System.Drawing.Size(157, 165);
+            this.txtPictureContact.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.txtPictureContact.TabIndex = 26;
+            this.txtPictureContact.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(577, 332);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(93, 20);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "Téléphone :";
+            // 
+            // txtTel
+            // 
+            this.txtTel.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTel.Location = new System.Drawing.Point(676, 328);
+            this.txtTel.Mask = "00 00 00 00";
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(127, 26);
+            this.txtTel.TabIndex = 24;
+            // 
+            // txtGenre
+            // 
+            this.txtGenre.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGenre.FormattingEnabled = true;
+            this.txtGenre.Items.AddRange(new object[] {
+            "M",
+            "F"});
+            this.txtGenre.Location = new System.Drawing.Point(676, 380);
+            this.txtGenre.Name = "txtGenre";
+            this.txtGenre.Size = new System.Drawing.Size(127, 28);
+            this.txtGenre.TabIndex = 23;
+            // 
+            // txtDateNaiss
+            // 
+            this.txtDateNaiss.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDateNaiss.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.txtDateNaiss.Location = new System.Drawing.Point(337, 382);
+            this.txtDateNaiss.Name = "txtDateNaiss";
+            this.txtDateNaiss.Size = new System.Drawing.Size(213, 26);
+            this.txtDateNaiss.TabIndex = 22;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(606, 383);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 20);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Genre :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(274, 435);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 20);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Email :";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(336, 432);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(213, 26);
+            this.txtEmail.TabIndex = 19;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(198, 383);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 20);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Date Naissance :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(212, 332);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 20);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Nom complet :";
+            // 
+            // txtNom
+            // 
+            this.txtNom.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNom.Location = new System.Drawing.Point(336, 330);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(213, 26);
+            this.txtNom.TabIndex = 16;
+            // 
+            // txtboxID
+            // 
+            this.txtboxID.AutoSize = true;
+            this.txtboxID.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboxID.Location = new System.Drawing.Point(626, 432);
+            this.txtboxID.Name = "txtboxID";
+            this.txtboxID.Size = new System.Drawing.Size(44, 20);
+            this.txtboxID.TabIndex = 31;
+            this.txtboxID.Text = "#ID :";
+            // 
+            // txtID
+            // 
+            this.txtID.Enabled = false;
+            this.txtID.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtID.Location = new System.Drawing.Point(676, 429);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(127, 26);
+            this.txtID.TabIndex = 30;
             // 
             // frmListContact
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.Controls.Add(this.txtboxID);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtPictureContact);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtTel);
+            this.Controls.Add(this.txtGenre);
+            this.Controls.Add(this.txtDateNaiss);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNom);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
             this.Controls.Add(this.dgvContacts);
             this.Controls.Add(this.nbContact);
             this.Controls.Add(this.pictureBox2);
@@ -88,6 +319,7 @@
             this.Load += new System.EventHandler(this.frmListContact_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvContacts)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPictureContact)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -99,5 +331,23 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label nbContact;
         private System.Windows.Forms.DataGridView dgvContacts;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.PictureBox txtPictureContact;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MaskedTextBox txtTel;
+        private System.Windows.Forms.ComboBox txtGenre;
+        private System.Windows.Forms.DateTimePicker txtDateNaiss;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.Label txtboxID;
+        private System.Windows.Forms.TextBox txtID;
     }
 }
--- AppEContact.WinForm/frmListContact.cs
+++ AppEContact.WinForm/frmListContact.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using AppEContact.WinForm.Models;
+using System.IO;
 
 namespace AppEContact.WinForm
 {
@@ -25,9 +26,58 @@ namespace AppEContact.WinForm
             dgvContacts.AutoResizeColumns();
             dgvContacts.AllowUserToResizeColumns = true;
             dgvContacts.AllowUserToOrderColumns = true;
-            //dgvContacts.Columns["photo"].Visible = false;
+            dgvContacts.Columns["photo"].Visible = false;
             dgvContacts.AllowUserToAddRows = false;
             nbContact.Text = "Nombre Contact : " + dgvContacts.Rows.Count;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (txtSearch.Text != "")
+            {
+                var lst = new BindingList<Contact>(DBContact.SearchContact(txtSearch.Text));
+                dgvContacts.DataSource = lst;
+            }
+            else
+            {
+                var lst = new BindingList<Contact>(DBContact.GetListContacts());
+                dgvContacts.DataSource = lst;
+            }
+
+            nbContact.Text = "Nombre contact : " + dgvContacts.Rows.Count;
+        }
+
+        private void txtSearch_KeyUp(object sender, KeyEventArgs e)
+        {
+            btnSearch_Click(sender, e);
+        }
+
+
+        private void dgvContacts_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                int index = e.RowIndex;
+                dgvContacts.Rows[index].Selected = true;
+                txtID.Text = dgvContacts.Rows[index].Cells[0].Value.ToString();
+                txtNom.Text = dgvContacts.Rows[index].Cells[1].Value.ToString();
+                string txtdate=dgvContacts.Rows[index].Cells[2].Value.ToString();
+                txtDateNaiss.Text = DateTime.Parse(txtdate).ToString("dd/MM/yyyy");
+                txtEmail.Text= dgvContacts.Rows[index].Cells[3].Value.ToString();
+                txtTel.Text = dgvContacts.Rows[index].Cells[4].Value.ToString();
+                txtGenre.Text = dgvContacts.Rows[index].Cells[5].Value.ToString();
+                byte[] img= (byte [])dgvContacts.Rows[index].Cells[6].Value;
+
+                if (img != null)
+                {
+                    MemoryStream ms = new MemoryStream(img);
+                    txtPictureContact.Image = Image.FromStream(ms);
+                }
+                else
+                {
+                    txtPictureContact.Image = null; 
+                }
+            }
         }
     }
 }
